44e205b3a51bc71bcf11b0c4aeb95a4321d493e6
du-ntomazela <161898042+du-ntomazela@users.noreply.github.com>
2024-09-30 17:57:34 -0300

    Sobre

--- my-project/src/pages/sobre.jsx
+++ my-project/src/pages/sobre.jsx
@@ -2,7 +2,13 @@ import "../App.css"
 
 function sobre () {
     return(
-        <h1>Sobre</h1>
+        <div className="flex-col text-center">
+            <h1 id="conteudo-main" className="text-6xl text-white mb-8">Sobre</h1>
+            <div className="flex justify-center mb-14">
+            <img className="w-2/6" src="../../public/imgs/perfil.png" alt="Eduardo Tomazela" />
+            </div>
+            <h1 className="text-2xl text-white mb-20 font-black">Olá, me chamo Eduardo Tomazela do Nascimento, tenho 18 anos, atualmente cursando o segundo semstre do curso de Engenharia de Software na fauldade FIAP. Atualmente estou alocado no cargo de Sales Intern na multinacional FiberHome, trabalhando 100% presencial, e praticando diariamente minhas habilidades em vendas, desenvolvimento, comunicação e inglês</h1>
+        </div>
     )
 }
 
