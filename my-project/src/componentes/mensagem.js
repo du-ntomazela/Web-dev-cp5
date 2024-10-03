9a67e2f4c06744d6ae58de82098d5c0bb934fce3
du-ntomazela <161898042+du-ntomazela@users.noreply.github.com>
2024-10-03 19:34:39 -0300

    arrumando imgs

--- my-project/src/componentes/mensagem.js
+++ my-project/src/componentes/mensagem.js
@@ -1,7 +1,7 @@
 function Msg () {
     alert("mensagem enviada com sucesso!")
     return(
-        window.location.reload()
+        window.location.href("https://web-dev-cp5.vercel.app/contato")
         
         
     )
