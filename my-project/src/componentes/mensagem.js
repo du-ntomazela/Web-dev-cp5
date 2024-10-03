be922d4da303a1d1d592ca78425840f55f07fab1
du-ntomazela <161898042+du-ntomazela@users.noreply.github.com>
2024-10-03 19:38:36 -0300

    Corrigindo erro de img

--- my-project/src/componentes/mensagem.js
+++ my-project/src/componentes/mensagem.js
@@ -1,9 +1,8 @@
 function Msg () {
     alert("mensagem enviada com sucesso!")
     return(
-        window.location.href("https://web-dev-cp5.vercel.app/contato")
-        
-        
+
+        location.reload(forceReload(True))
     )
 }
 export default Msg
